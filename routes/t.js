3837355d71aee3fe3bf2ab791e759b2cd2583bac
misaniuk <denis.misaniuk@gmail.com>
2020-12-19 01:15:40 +0200

    +

--- routes/t.js
+++ routes/t.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const translate = require('@vitalets/google-translate-api');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource --->' + res);
+  translate(req.query.text, {to: req.query.to, from: req.query.from, client: 'gtx'}).then(r => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(r.text);
+
+}).catch(err => {
+    console.error(err);
+});
+
 });
 
 module.exports = router;
